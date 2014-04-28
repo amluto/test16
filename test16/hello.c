c7d69efacf81a8f83687235d9fe57a3816460791
H. Peter Anvin <hpa@linux.intel.com>
2014-04-28 13:49:15 -0700

    hello.c: try to provoke an error on fouling of the high bits of %esp
    To try to help track down the problem with the high bits of %esp,
    explicitly try to error out on fouling.
    
    Signed-off-by: H. Peter Anvin <hpa@linux.intel.com>

--- test16/hello.c
+++ test16/hello.c
@@ -1,7 +1,31 @@
 extern void puts(const char *);
 
+static const char *hexstr(unsigned int v)
+{
+	const char hexdigits[] = "0123456789abcdef";
+	static char hexbuf[9];
+	char *p = hexbuf;
+
+	for (i = 0; i < 8; i++) {
+		*p++ = hexdigits[v >> 28];
+		v <<= 4;
+	}
+	*p = '\0';
+
+	return hexbuf;
+}
+
+static inline unsigned int esp(void)
+{
+	unsigned int v;
+	asm volatile("movl %%esp,%0 ; movzwl %%sp,%%esp" : "=rm" (v));
+	return v;
+}
+
 int main(void)
 {
-	puts("Hello, World!\n");
+	puts("Hello, World, esp = %\n");
+	asm("movl (%%esp),%%eax" : : : "eax");
+	puts("Hello, Afterworld!\n");
 	return 0;
 }
